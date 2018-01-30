7c1a8088d8e42f03b3f0613cb312d763119edb24
Mohammed Rahman <m2rahman@ucsd.edu>
2018-01-29 23:39:38 -0800

    Added layout basics

---
+++ flix demo/NowPlayingViewController.swift
@@ -0,0 +1,77 @@
+//
+//  NowPlayingViewController.swift
+//  flix demo
+//
+//  Created by Mo on 1/29/18.
+//  Copyright © 2018 Mo. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController, UITableViewDataSource{
+
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        
+        tableView.dataSource = self
+        
+        //TODO why does the line below work to change the rowHeight but not the storyboard 
+        tableView.rowHeight = 175
+        
+        super.viewDidLoad()
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies{
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+            }
+        }
+        task.resume()
+        
+    }
+/*
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
+    }
+    
+    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
