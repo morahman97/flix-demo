e833d4ad355a1b3f1a423597185633b612dd2916
Mohammed Rahman <m2rahman@ucsd.edu>
2018-02-06 17:26:00 -0800

    Finished main user stories

---
+++ flix demo/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix demo
+//
+//  Created by Mo on 2/6/18.
+//  Copyright © 2018 Mo. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
